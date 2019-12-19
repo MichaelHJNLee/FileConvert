be86f3b12862a5b16616526214f7ff470e560951
Michael Lee <michaellee@Michaels-MacBook-Pro-2.local>
2019-12-18 21:45:46 -0800

    added styling and security

--- config/filestack.js
+++ config/filestack.js
@@ -1,5 +1,9 @@
 var apiKey = 'AyGKqfXxOSpWPqbkhtsrYz';
+var sec = 'fa6f323219622286c6de2d789771bc7bae0e9f93f992229ed8ecfcafa62b7f0a';
+var pol = 'eyJleHBpcnkiOjE1NzkyNDgwMDB9';
 
 module.exports = {
-  apiKey
+  apiKey,
+  sec,
+  pol
 }
--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const filestack = require('filestack-js');
-const key = require('../config/filestack.js');
 const fs = require('fs');
+const key = require('../config/filestack.js');
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'images/');
@@ -15,7 +15,10 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 const app = express();
 const port = 3000;
-const client = filestack.init(key.apiKey);
+const options = { "security": {} };
+options["security"]["policy"] = key.pol;
+options["security"]["signature"] = key.sec;
+const client = filestack.init(key.apiKey, options);
 
 app.use(express.static('dist'));
 
@@ -23,12 +26,15 @@ app.use(express.static('dist'));
 app.post('/api/image/:type', upload.single('image'), (req, res, next) => {
   client.upload(__dirname + `/../images/${req.file.filename}`)
     .then((response) => {
-      let src = new filestack.Filelink(response.handle, key.apiKey);
-      src.output({"format": `${req.params.type}`});
-      let data = src.toString();
-      res.send(data)
+      let transformed = client.transform(response.handle, {output: {"format": req.params.type}});
+      res.send(transformed)
+      // let src = new filestack.Filelink(response.handle, key.apiKey);
+      // src.output({"format": `${req.params.type}`});
+      // let data = src.toString();
+      // res.send(data)
     })
     .catch((err) => {
+      console.log(err, options)
       res.send(err);
     })
     .then(() => {
