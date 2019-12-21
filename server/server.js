565490cd92faffb7752017567f3163119ab9feca
Michael Lee <michaellee@Michaels-MacBook-Pro-2.local>
2019-12-21 10:10:44 -0800

    reformatted

--- server/server.js
+++ server/server.js
@@ -4,52 +4,42 @@ const key = require('../config/filestack.js');
 const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
 const request = require('request');
-const app = express();
+
+const app = express(); 
 const port = 3000;
-const options = { security: {} };
-options['security']['policy'] = key.pol;
-options['security']['signature'] = key.sec;
+const options = { "security": {} };
+options["security"]["policy"] = key.pol;
+options["security"]["signature"] = key.sec;
 const client = filestack.init(key.apiKey, options);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended : false}));
 app.use(fileUpload());
 app.use(express.static('dist'));
 
 app.post('/api/verify', (req, res) => {
   if (req.body.value) {
-    request(
-      `https://www.google.com/recaptcha/api/siteverify?secret="${key.capsec}"&response="${req.body.value}"`,
-      (err, response, body) => {
-        if (err || (body.success !== undefined && !body.success)) {
-          return res.json({
-            responseCode: 1,
-            responseDesc: 'Failed captcha verification',
-          });
-        }
-        res.json({ responseCode: 0, responseDesc: 'Success' });
-      },
-    );
+    request(`https://www.google.com/recaptcha/api/siteverify?secret="${key.capsec}"&response="${req.body.value}"`, (err, response, body) => {
+      if(err || (body.success !== undefined && !body.success)) {
+        return res.json({"responseCode" : 1, "responseDesc" : "Failed captcha verification"});
+      }
+      res.json({"responseCode" : 0, "responseDesc" : "Success"});
+    })
   } else {
-    return res.json({
-      responseCode: 1,
-      responseDesc: 'Failed captcha verification',
-    });
+    return res.json({"responseCode" : 1, "responseDesc" : "Failed captcha verification"});
   }
-});
+})
 
 app.post('/api/image/:type', (req, res) => {
   client
     .upload(req.files.image.data)
     .then(response => {
-      let transformed = client.transform(response.handle, {
-        output: { format: req.params.type },
-      });
-      res.send(transformed);
+      const transformed = client.transform(response.handle, { output: {"format": req.params.type }});
+      res.send(transformed)
     })
     .catch(err => {
       res.send(err);
-    });
-});
+    })
+})
 
 app.listen(port, console.log(`Listening on port ${port}`));
